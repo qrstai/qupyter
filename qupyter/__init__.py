4b469aaf330acbb7f002ae73fdb1ad8badde38cc
Jihoon Kang <kang@ghoon.net>
2023-11-01 18:52:20 +0900

    feat: update init.py

--- qupyter/__init__.py
+++ qupyter/__init__.py
@@ -1,7 +1,8 @@
 from . import futures
 from . import stocks
 from . import brokerage
+from . import test
 
-__all__ = ['futures', 'stocks', 'brokerage']
+__all__ = ['futures', 'stocks', 'brokerage', 'test']
 
 __version__ = '0.0.1'
